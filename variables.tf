2ed321990a6d9f44f73ccdf0dd4758a3dec99c0f
gforsack <48193981+gforsack@users.noreply.github.com>
2022-10-25 21:35:17 -0400

    Update and rename terraform.tfvars to variables.tf

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "rootdomain" {
+}
+
+variable "subdomain" {
+}
+
+variable "dynamodb_table" {
+  description = "Name of the DynamoDB table"
+  type        = string
+  default     = "visitorCountDB"
+}
+
+variable "lambda_name" {
+  description = "Name of the lambda function"
+  type        = string
+  default     = "lambda-terraform"
+}
+
+variable "apigw_name" {
+  description = "Name of REST API Gateway Resource"
+  type        = string
+  default     = "invokeLambdaAPI-terraform"
+}
+
+variable "stage_name" {
+  description = "Deployment stage"
+  type        = string
+  default     = "prod"
+}
+
+variable "zone_ID" {
+}
